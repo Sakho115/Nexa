bb57c76aeeced555dc97c5172da4125cf021b9f0
Sakho115 <152713328+Sakho115@users.noreply.github.com>
2025-06-28 21:36:04 +0530

    Added package-lock.json

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nexa Final</h1>
+        <p>Working Frontend and Backend Application</p>
+        <p>Ready for Hackathon Deployment!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
